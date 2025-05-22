d00a739bdf84f662381cbb99d1107d36cc774da1
Rishackmoto <rishackmoto@gmail.com>
2025-05-22 17:25:37 +0900

    Update API ayam - perbaikan login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config()
 const { poolPromise } = require("./db");
 
 const authRoutes = require('./routes/auth');
