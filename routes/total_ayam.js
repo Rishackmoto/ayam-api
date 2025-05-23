5a91b5901606e671a4bd57f74baac9969149dd19
Rishackmoto <rishackmoto@gmail.com>
2025-05-23 16:35:49 +0900

    Update API ayam - perbaikan login

--- routes/total_ayam.js
+++ routes/total_ayam.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { sql, poolPromise } = require('../db'); // sesuaikan dengan file koneksi kamu 
 
 // GET semua jenis tarif
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const pool = await poolPromise;
     const { jns, tgl } = req.body;
