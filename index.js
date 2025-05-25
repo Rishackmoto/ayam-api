99effab637fe13eec15afec8f764aaeddf79f16a
Rishackmoto <rishackmoto@gmail.com>
2025-05-25 10:50:46 +0900

    Update API ayam - perbaikan login

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   const host = req.headers.host;
   if (host === 'ayam-api-nine.vercel.app') {
-    return res.redirect(301, `https://app.rishackmoto.com${req.url}`);
+    return res.redirect(301, `https://app.sit-loor.com${req.url}`);
   }
   next(); // lanjutkan ke route berikutnya
 });
