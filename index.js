21ce52652cb197bfeec3bfa89375c045e9e746dd
Rishackmoto <rishackmoto@gmail.com>
2025-05-25 09:35:19 +0900

    Update API ayam - perbaikan login

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors({
   origin: [
     "https://ayam-api.up.railway.app",
     "https://api.rishackmoto.com",
-    "https://ayam-api-nine.vercel.app"
+    // "https://ayam-api-nine.vercel.app"
   ],
   credentials: true
 }));
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   const host = req.headers.host;
   if (host === 'ayam-api-nine.vercel.app') {
-    return res.redirect(301, `https://app.rishackmoto.com${req.url}`);
+    return res.redirect(301, `https://api.rishackmoto.com${req.url}`);
   }
   next(); // lanjutkan ke route berikutnya
 });
